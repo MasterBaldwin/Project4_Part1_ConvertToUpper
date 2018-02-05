4415b9cb316fea15cac0123e2fe29f8cc4ab58c2
Mike Baldwin <michael.baldwin3@rockets.utoledo.edu>
2018-02-05 15:13:42 -0500

    Added JPanel stuffs.

--- src/ConvertToUpper.java
+++ src/ConvertToUpper.java
@@ -4,21 +4,22 @@ import java.util.Scanner;
 public class ConvertToUpper {
 public static void main(String[] args) {
     //JOptionPane panel = new JOptionPanel()
-    Scanner inputStream = new Scanner(System.in);
+    //Scanner inputStream = new Scanner(System.in);
     String charInput;
 
-    JOptionPane.showInputDialog("Enter a letter: ");
+    charInput = JOptionPane.showInputDialog("Enter a letter: ");
 
-    System.out.print("Enter a letter: ");
-    charInput = "";
+    //System.out.print("Enter a letter: ");
 
     if(charInput.matches(".*\\d.*"))
     {
-        System.out.println("Not a letter.");
+        JOptionPane.showMessageDialog(null, "Not a letter");
+       // System.out.println("Not a letter.");
     }
     else
     {
-        System.out.println("The uppercase equivalent of " + charInput + " is " + charInput.toUpperCase());
+        JOptionPane.showMessageDialog(null, "The uppercase equivalent of " + charInput + " is " + charInput.toUpperCase());
+        //System.out.println("The uppercase equivalent of " + charInput + " is " + charInput.toUpperCase());
     }
 }
 }
