4a66fb57920d3ec248558e67fc0d21fd314c28f6
Mike Baldwin <Michael.Baldwin3@rockets.utoledo.edu>
2018-02-06 22:40:57 -0500

    Added a header comment and reformatted some code.

--- src/ConvertToUpper.java
+++ src/ConvertToUpper.java
@@ -1,7 +1,11 @@
 import javax.swing.*;
-import java.util.Scanner;
 
 public class ConvertToUpper {
+// Written by: Mike Baldwin
+// Computes the average of a set of values entered by the user, e.g. with
+//     10.0 5.0 6.0 9.0 0.0
+// The average is 7.5
+
 public static void main(String[] args) {
     //JOptionPane panel = new JOptionPanel()
     //Scanner inputStream = new Scanner(System.in);
@@ -11,13 +15,10 @@ public static void main(String[] args) {
 
     //System.out.print("Enter a letter: ");
 
-    if(charInput.matches(".*\\d.*"))
-    {
+    if (charInput.matches(".*\\d.*")) {
         JOptionPane.showMessageDialog(null, "Not a letter");
-       // System.out.println("Not a letter.");
-    }
-    else
-    {
+        // System.out.println("Not a letter.");
+    } else {
         JOptionPane.showMessageDialog(null, "The uppercase equivalent of " + charInput + " is " + charInput.toUpperCase());
         //System.out.println("The uppercase equivalent of " + charInput + " is " + charInput.toUpperCase());
     }
