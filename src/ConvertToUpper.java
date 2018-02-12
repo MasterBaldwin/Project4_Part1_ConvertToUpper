19a1c46e7be6ca1d2a07bccb849514aa60745164
Mike Baldwin <mikebaldwin21@gmail.com>
2018-02-12 15:16:16 -0500

    Formatting, brace removals, and text editing.

--- src/ConvertToUpper.java
+++ src/ConvertToUpper.java
@@ -2,25 +2,19 @@ import javax.swing.*;
 
 public class ConvertToUpper {
 // Written by: Mike Baldwin
-// Computes the average of a set of values entered by the user, e.g. with
-//     10.0 5.0 6.0 9.0 0.0
-// The average is 7.5
+// Converts a lower case letter to an uppercase letter
 
 public static void main(String[] args) {
-    //JOptionPane panel = new JOptionPanel()
-    //Scanner inputStream = new Scanner(System.in);
     String charInput;
 
-    charInput = JOptionPane.showInputDialog("Enter a letter: ");
+    charInput = JOptionPane.showInputDialog(null,
+            "Enter a letter", "Convert To Upper", JOptionPane.QUESTION_MESSAGE);
 
-    //System.out.print("Enter a letter: ");
-
-    if (charInput.matches(".*\\d.*")) {
+    if (Character.isLetter(charInput.charAt(0))) {
+        JOptionPane.showMessageDialog(null,
+                "The uppercase equivalent of " + charInput + " is " +
+                        charInput.toUpperCase());
+    } else
         JOptionPane.showMessageDialog(null, "Not a letter");
-        // System.out.println("Not a letter.");
-    } else {
-        JOptionPane.showMessageDialog(null, "The uppercase equivalent of " + charInput + " is " + charInput.toUpperCase());
-        //System.out.println("The uppercase equivalent of " + charInput + " is " + charInput.toUpperCase());
-    }
 }
 }
